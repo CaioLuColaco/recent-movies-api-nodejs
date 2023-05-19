4010c8b48d7df9144fafc685ab82d1d0c703e8b5
caio lucena <caio.lcolaco@gmail.com>
2023-05-18 22:31:59 -0300

    Created a middleware to validate the token before execute a controller

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,16 @@ const routes = express.Router()
 import {createUser, loginUser} from './controllers/UserControllers.js'
 import { getBrazilianMovies, likeMovie, getOneMovie, getAllLikedMovies } from "./controllers/MoviesControllers.js"
 
+import { checkUserToken } from "./middlewares/checkToken.js";
+
 // User Routes
 routes.post("/registerUser", createUser)
 routes.post("/loginUser", loginUser)
 
 // Movies Routes
 routes.get("/tenMovies", getBrazilianMovies)
-routes.get("/movie/:title", getOneMovie)
-routes.get("/movies", getAllLikedMovies)
-routes.put("/likeMovie/:title", likeMovie)
+routes.get("/movie/:title", checkUserToken, getOneMovie)
+routes.get("/movies", checkUserToken, getAllLikedMovies)
+routes.put("/likeMovie/:title", checkUserToken,likeMovie)
 
 export default routes;
