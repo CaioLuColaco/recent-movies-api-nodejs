50b63ad3cbafe39087f51ca09a6d077d7e835351
caio lucena <caio.lcolaco@gmail.com>
2023-05-18 23:22:32 -0300

    removed dev consoles

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken"
 export function checkUserToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    console.log(token)
     if(!token) {
         return res.status(401).json({message: "Acesso negado!"})
     }
